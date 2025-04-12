f8b3357895821e460e0a869f8a661b2f6fb00084
Yuvraj-Lolage <yuvilolage7@gmail.com>
2025-04-12 14:31:34 +0530

    added hosted backend link

--- src/API/axiosInstance.js
+++ src/API/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL:"http://localhost:8443",
-    baseURL:"https://wedstra-backend-9886.onrender.com/",
+    baseURL:"https://wedstra-backend-9886.onrender.com",
     headers:{
         "Content-Type":"application/json"
     },
